1e90593cd60cd9b81930b1f4c91029c71ea2f027
Cyril Carail <carail.cyril@gmail.com>
2018-09-30 11:33:42 +0200

    Learning, 1st time using github

---
+++ FORMATION UDACITY/WEB DEV/LESSON 2/play.py
@@ -0,0 +1,21 @@
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+def valid_month(month):
+
+    if month:
+        cap_month = month.capitalize()
+        if month in months
+    return cap_month
+    
+print valid_month("january")
